92df618fba75f096607aacc4b1255c713c7a3a44
noobDevUmar <m.umarqureshi091@gmail.com>
2024-01-25 10:32:59 -0800

    Messages added in db

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -1,16 +1,43 @@
 import { Button } from '@mui/material'
-import React from 'react'
+import { collection } from 'firebase/firestore'
+import React, { useRef, useState } from 'react'
+import { db } from '../firebase'
+import { doc, getDoc,addDoc } from 'firebase/firestore';
+import { serverTimestamp } from 'firebase/firestore';
+
+
 
 function ChatInput({channelId,channelName}) {
+const [inputMessge,setInputMessage] = useState('')
+ 
 
-    const sendAmessage=(e)=>{
+    const sendAmessage=async(e)=>{
         e.preventDefault();
 
+        const channelRef = doc(db, 'rooms', channelId);
+      
+          const messagesRef = collection(db, 'rooms', channelId, 'messages');
+       
+         
+          await addDoc(messagesRef, {
+            message: inputMessge,
+            createdAt: serverTimestamp()
+            // include other message fields like timestamp, user info, etc.
+          });
+      
+          console.log('Message added!');
+          setInputMessage('')
+    
+     
     }
+
+
   return (
     <div className="rounded-xl ">
       <form className="flex relative justify-center">
         <input
+        value={inputMessge}
+        onChange={(e)=>setInputMessage(e.target.value)}
           placeholder={`Message #${channelId}`}
           className="fixed bottom-8 w-[60%] rounded-md p-5 outline-none border border-gray-300"
         />
