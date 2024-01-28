a1dce0852bae0f005f68f6b32011d2995f1f14d4
noobDevUmar <m.umarqureshi091@gmail.com>
2024-01-28 00:52:53 -0800

    delete room added

--- src/components/SideBarOption.jsx
+++ src/components/SideBarOption.jsx
@@ -1,35 +1,17 @@
 import React from 'react'
 import InsertComment from '@mui/icons-material/InsertComment';
 import { auth, db } from '../firebase';
-import {  addDoc, collection, getDocs, serverTimestamp } from 'firebase/firestore';
+import {  addDoc, collection, deleteDoc, doc, getDocs, serverTimestamp } from 'firebase/firestore';
 import { useDispatch } from 'react-redux';
 import { enterRoom } from '../redux/appSlice';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
 
-function SideBarOptions({Icon,title,addChannelOption,id}) {
+function SideBarOptions({Icon,title,addChannelOption,id,isCreator}) {
 const dispatch = useDispatch();
-const roomsCollectionRef = collection(db, "rooms");   // Reference to the 'rooms' collection
 const [user] = useAuthState(auth)
 
 
-
-
-const addChannel = () => {
-  // todo : instead add a modal for asking this and return the error if criteria doesn't match
-  const channelName = prompt("Please enter a Room Name");
-
-  if (channelName.length < 4) return;  // Ensuring the channel name meets a minimum length requirement
-
-  addDoc(roomsCollectionRef, {
-    name: channelName,
-    createdAt: serverTimestamp(),
-    creator: user.uid,  // Stores who made this room
-    members: {
-      [user.uid]: true  // Initialize the creator as a member
-    }
-  });
-};
 
 
 const selectChannel =()=>{
@@ -40,22 +22,49 @@ const selectChannel =()=>{
   }
 }
 
+
+const leavRoom = ()=>{
+  console.log("Leave");
+}
+
+
+const deleteRoom = async (roomId, e) => {
+  e.stopPropagation(); // Stop event propagation to prevent triggering selectChannel
+  try {
+    dispatch(enterRoom({ roomId: null })); // Update state after successful deletion
+    const roomRef = doc(db, "rooms", roomId);
+    await deleteDoc(roomRef);
+    console.log(`Room with ID ${roomId} has been deleted.`);
+  } catch (error) {
+      console.error("Error deleting room: ", error);
+    }
+};
+
+
+
   return (
     <div className='flex text-sm items-center pl-[2px] p-1 cursor-pointer hover:bg-[#340e36] hover:opacity-90 ml-4 gap-2'
-    onClick={addChannelOption ? addChannel : selectChannel}
+   
     >
-
-        
-{Icon && <Icon fontSize="small" style={{padding:1}}  /> }
 {
-  Icon ? (
-    <h3 className='text-lg font-semibold'> {title} </h3>
-  ) : (
-    <div className="div px-2 text-lg font-light">
-      <span className='p-4'>#</span> {title}
-    </div>
-  )
-}
+      Icon ? (
+        <div className="flex items-center">
+          <Icon fontSize="small" style={{ padding: 1 }} />
+          <h3 className='text-lg font-semibold'> {title} </h3>
+        </div>
+      ) : (
+        <div className="div px-2 text-lg font-light flex justify-around  w-full items-center" onClick={selectChannel}> 
+        <h3 className='p-1'>#</h3> 
+        <div className='flex-1'>{title}</div> {/* Wrap the title */}
+        {isCreator ? (
+         <button onClick={(event) => deleteRoom(id, event)} className='text-red-600 h-full text-sm hover:text-lg'>Delete</button>
+        ) :
+        <button onClick={leavRoom} className='text-red-600 text-sm hover:text-lg '> Leave </button>
+        }
+      </div>
+      
+      )
+    }
 
     </div>
   )
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ import CreateIcon from '@mui/icons-material/Create';
 import SideBarOption from './SideBarOption';
 import InsertComment from '@mui/icons-material/InsertComment';
 import { Apps, BookmarkBorder, Drafts, ExpandLess, ExpandMore, FileCopy, Inbox, PeopleAlt } from '@mui/icons-material';
-import {  addDoc, collection, getDocs, query, where } from 'firebase/firestore';
+import {  addDoc, collection, getDocs, query, serverTimestamp, where } from 'firebase/firestore';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import { auth, db } from '../firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
@@ -17,22 +17,36 @@ function Sidebar() {
   const roomsQuery = query(
     roomsCollectionRef,
     where(`members.${user.uid}`, '==', true),  // Check if the user is a member
-    where('creator', '==', user.uid)           // OR check if the user is the creator
+    // where('creator', '==', user.uid)   ,        // OR check if the user is the creator
   );
   
   // Use the Firebase hook to listen to the collection
   const [channels, loading, error] = useCollection(roomsQuery);
- const [isShowMoreOpen , SetShowMoreOpen] = useState(false)
+  const [isShowMoreOpen , SetShowMoreOpen] = useState(false)
 
   
+  const addChannel = () => {
+    // todo : instead add a modal for asking this and return the error if criteria doesn't match
+    const channelName = prompt("Please enter a Room Name");
+  
+    if (channelName.length < 4) return;  // Ensuring the channel name meets a minimum length requirement
+  
+    addDoc(roomsCollectionRef, {
+      name: channelName,
+      createdAt: serverTimestamp(),
+      creator: user.uid,  // Stores who made this room
+      members: {
+        [user.uid]: true  // Initialize the creator as a member
+      }
+    });
+  };
 
   return (
-    // container
-
     <div  style={{ flex: 0.3, borderTop: '1px solid #49274b', overflow: 'hidden' }}
     className="bg-slack mt-[56px] text-white h-full overflow-y-auto">
     
-      {/* Header */}
+                     {/* Header */}
+
       <div style={{ borderBottom: `1px solid #49274b` }} className=" flex p-3 mt-12">
         <div className="info flex-1 ">
           <h1 className="text-2xl font-bold mb-1 ml-1"> Dev Flex </h1>
@@ -44,8 +58,9 @@ function Sidebar() {
         <CreateIcon className="bg-white p-1 rounded-full text-2xl text-[#49274b]" />
       </div>
 
-      {/* Sidebar Options */}
+                      {/* Sidebar Options */}
 
+      {/* dummy data for now */}
       <SideBarOption Icon={InsertComment} title="Threads"/>
       <SideBarOption Icon={Inbox} title="Mentions & Reactions"/>
 
@@ -59,25 +74,21 @@ function Sidebar() {
           </>
         ):  
         <button onClick={()=> SetShowMoreOpen(true)} className='pl-3 h-12 text-lg'> {<ExpandMore/>} Show More </button>
-      
         }
       
 
-      
-      
- 
-
       <hr className='opacity-20 my-2'/>
 
-      <SideBarOption addChannelOption Icon={Add} title={'Add Channel'}/>
-     
+   
+          <button className='text-lg font-bold w-full' onClick={addChannel}>  {<Add  fontSize="small" style={{ padding: 1 }}/>} Add Channel </button>
       {channels?.docs.map((doc)=>(
         <SideBarOption
         key={doc.id}
         id={doc.id}
-        title={doc.data().name}/>
+        title={doc.data().name}
+        isCreator={user.uid === doc.data().creator}
+        />
       ))}
-
 
     </div>
   );
